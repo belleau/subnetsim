9719aa50aed216c868a63dc37b6bad87b412e2d3
adeschen <adeschen@hotmail.com>
2018-07-26 22:52:32 -0400

    Update unit tests

--- tests/testthat/test-subnetsimInternalMethods.R
+++ tests/testthat/test-subnetsimInternalMethods.R
@@ -31,7 +31,8 @@ test_that("simuleSubNet() return predicted results when seed fixed", {
                  data.frame(nbNodes=c(134, 149, 129, 135),
                             nbLink=c(213, 257, 242, 205),
                             nbLinkSub=c(13, 13, 16, 8)))
-    expect_equal(simuleSubNet(IRGEN_netAll, IRGEN_nodesAll, 4, 12, 145),
+    expect_equal(simuleSubNet(IRGEN_netAll, IRGEN_nodesAll, as.integer(4),
+                                as.integer(12), 145),
                  data.frame(nbNodes=c(73, 32, 50, 61),
                             nbLink=c(98, 44, 56, 78),
                             nbLinkSub=c(4, 0, 0, 3)))

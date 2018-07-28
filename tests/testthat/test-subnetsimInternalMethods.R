c259b3682cfd18598dea3d43fead8ff1e51f6721
adeschen <adeschen@hotmail.com>
2018-07-27 23:45:39 -0400

    Correct unit tests related to simuleSubNet()

--- tests/testthat/test-subnetsimInternalMethods.R
+++ tests/testthat/test-subnetsimInternalMethods.R
@@ -28,20 +28,20 @@ context("simuleSubNet() results")
 
 test_that("simuleSubNet() return predicted results when seed fixed", {
     expect_equal(simuleSubNet(IRGEN_netAll, IRGEN_nodesAll, 4, 33, 145),
-                 data.frame(nbNodes=c(134, 149, 129, 135),
+                 data.frame(nbNodes=c(153, 166, 145, 157),
                             nbLink=c(213, 257, 242, 205),
                             nbLinkSub=c(13, 13, 16, 8)))
     expect_equal(simuleSubNet(IRGEN_netAll, IRGEN_nodesAll, as.integer(4),
                                 as.integer(12), 145),
-                 data.frame(nbNodes=c(73, 32, 50, 61),
+                 data.frame(nbNodes=c(80, 44, 62, 68),
                             nbLink=c(98, 44, 56, 78),
                             nbLinkSub=c(4, 0, 0, 3)))
     expect_equal(simuleSubNet(IRGEN_netAll, IRGEN_nodesAll, 3, 12, 333),
-                 data.frame(nbNodes=c(85, 66, 75),
+                 data.frame(nbNodes=c(95, 76, 85),
                             nbLink=c(102, 79, 89),
                             nbLinkSub=c(1, 1, 1)))
     expect_equal(simuleSubNet(IRGEN_netAll, IRGEN_nodesAll, 5, 22, 1111),
-                 data.frame(nbNodes=c(111, 113, 102, 81, 122),
+                 data.frame(nbNodes=c(122, 130, 118, 97, 132),
                             nbLink=c(177, 143, 166, 116, 179),
                             nbLinkSub=c(7, 3, 5, 4, 7)))
 })

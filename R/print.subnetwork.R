91ce48a2ddd22c5d949d496ea922a13797687035
adeschen <adeschen@hotmail.com>
2018-08-01 00:22:30 -0400

    Update print.subnetwork() function

--- R/print.subnetwork.R
+++ R/print.subnetwork.R
@@ -3,13 +3,19 @@
 #' @param x the output object from \code{subnetwork} function to be printed
 #' @param \ldots arguments passed to or from other methods
 #' @export
-print.subnetwork <- function(x, ...) {
-    # Print title before printing the content of the regression object
+print.subnetwork <- function(x, digits=4,...) {
+    # Print title before printing the content of the subnetwork object
     cat("TODO\n")
     cat("TODO\n\n")
     cat("Iterations:\n")
     print(x$nbIter, ...)
     cat("Seed:\n")
     print(x$seed, ...)
+    cat("Number of nodes for simulated subnetworks plus one link:\n")
+    print(summary(x$nbNodesOneLink), ...)
+    cat("Number of links for simulated subnetworks plus one link:\n")
+    print(summary(x$nbLinkOneLink), ...)
+    cat("Number of links in the simulated subnetworks:\n")
+    print(summary(x$nbLink), ...)
     invisible(x)
 }

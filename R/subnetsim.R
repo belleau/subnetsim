52387fca75e43129f7723f1bc810e8395f663901
Astrid Deschenes <adeschen@hotmail.com>
2018-08-02 20:09:03 -0400

    Update package description

--- R/subnetsim.R
+++ R/subnetsim.R
@@ -5,9 +5,9 @@
 #' a subnetwork is significantly higher that the number obtained by a
 #' stochastic effect with a subnetwork with the same number of node.
 #'
-#' This package tests if a subnetwork is more connected than a randomly
-#' selected subnetwork with the same number of nodes. We validate three
-#' measures of connectivity: the number of links starting from the nodes
+#' This package tests the hypothesis that a specific subnetwork is more connected
+#' than randomly selected subnetworks with the same number of nodes. Three
+#' measures of connectivity are validated: the number of links starting from the nodes
 #' of the subnetwork, the number of nodes plus the number of nodes
 #' directly connected to the subnetwork and the number of links from nodes
 #' of the subnetwork and the nodes directly connected to the subnetwork.

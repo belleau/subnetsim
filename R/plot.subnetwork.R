d05dd712d30708b35fa4a9fb2bb42bf54f787a3c
Astrid Deschenes <adeschen@hotmail.com>
2018-08-02 22:57:33 -0400

    Update output of plot.subnetwork()

--- R/plot.subnetwork.R
+++ R/plot.subnetwork.R
@@ -6,15 +6,13 @@
 #' @export
 plot.subnetwork <- function(x, bins=30, ...) {
     # Print graph of the subnetwork object
-    nbNodes=272
-    df <- data.frame(nbNodes = nbNodes)
     data <- data.frame(nbNodesOneLink=x$nbNodesOneLink,
                        nbLinkOneLink = x$nbLinkOneLink,
                        nbLink = x$nbLink)
     graph01 <- ggplot(data, aes(x=nbNodesOneLink)) +
         geom_histogram(colour = "black",fill = c("black"), bins=bins) +
         xlab("Number of nodes of the networks including first degree neighbors") + ylab("Count") +
-        geom_vline(aes(xintercept = nbNodes, color = "observed"),
+        geom_vline(aes(xintercept = x$nbNodesOneLinkTested, color = "observed"),
                    linetype="longdash", size = 1.2, show.legend=TRUE) +
         scale_color_manual(name = "", values = c(observed = "red"))+
         theme(legend.position="top")
@@ -23,15 +21,15 @@ plot.subnetwork <- function(x, bins=30, ...) {
 
     graph02 <- ggplot(data, aes(x=nbLinkOneLink)) +
         geom_histogram(colour = "black",fill = c("black"), bins=bins) +
-        xlab("Number of links of the networks including first degree neighbors") + ylab("Count") +
-        geom_vline(aes(xintercept = nbNodes, color = "observed"),
+        xlab("Number of links of the networks including first-degree neighbors") + ylab("Count") +
+        geom_vline(aes(xintercept = x$nbLinkOneLinkTested, color = "observed"),
                    linetype="longdash", size = 1.2, show.legend=FALSE) +
         scale_color_manual(name = "", values = c(observed = "red"))
 
     graph03 <- ggplot(data, aes(x=nbLink)) +
         geom_histogram(colour = "black",fill = c("black"), bins=bins) +
         xlab("Number of links of the networks") + ylab("Count") +
-        geom_vline(aes(xintercept = nbNodes, color = "observed"),
+        geom_vline(aes(xintercept = x$nbLinkTested, color = "observed"),
                    linetype="longdash", size = 1.2, show.legend=FALSE) +
         scale_color_manual(name = "", values = c(observed = "red")) +
         theme(legend.position="bottom")

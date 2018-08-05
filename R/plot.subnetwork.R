dd05a7cfb195abcd5ebcb9240ea982cb8a8040ec
Astrid Deschenes <adeschen@hotmail.com>
2018-08-05 19:25:37 -0400

    Update plot() function

--- R/plot.subnetwork.R
+++ R/plot.subnetwork.R
@@ -1,7 +1,7 @@
 #' @rdname subnetwork
 #' @method plot subnetwork
 #' @param bins the number of bins used to generate the histograms
-#' @importFrom ggplot2 ggplot geom_histogram geom_vline aes xlab ylab scale_color_manual theme
+#' @importFrom ggplot2 ggplot geom_histogram geom_vline aes xlab ylab scale_color_manual theme ggplot_build ggplot_gtable
 #' @importFrom gridExtra grid.arrange
 #' @export
 plot.subnetwork <- function(x, bins=30, ...) {
@@ -9,15 +9,22 @@ plot.subnetwork <- function(x, bins=30, ...) {
     data <- data.frame(nbNodesOneLink=x$nbNodesOneLink,
                        nbLinkOneLink = x$nbLinkOneLink,
                        nbLink = x$nbLink)
-    graph01 <- ggplot(data, aes(x=nbNodesOneLink)) +
+
+    graphTEMP <- ggplot(data, aes(x=nbNodesOneLink)) +
         geom_histogram(colour = "black",fill = c("black"), bins=bins) +
         xlab("Number of nodes of the networks including first degree neighbors") + ylab("Count") +
         geom_vline(aes(xintercept = x$nbNodesOneLinkTested, color = "observed"),
                    linetype="longdash", size = 1.2, show.legend=TRUE) +
-        scale_color_manual(name = "", values = c(observed = "red"))+
+        scale_color_manual(name = "", values = c(observed = "red")) +
         theme(legend.position="top")
-    #geom_vline(xintercept = nbNodes, colour="red", linetype = "longdash",
-    #           size = 1.5, show.legend=TRUE) +
+
+    graph01 <- ggplot(data, aes(x=nbNodesOneLink)) +
+        geom_histogram(colour = "black",fill = c("black"), bins=bins) +
+        xlab("Number of nodes of the networks including first degree neighbors") + ylab("Count") +
+        geom_vline(aes(xintercept = x$nbNodesOneLinkTested, color = "observed"),
+                   linetype="longdash", size = 1.2, show.legend=FALSE) +
+        scale_color_manual(name = "", values = c(observed = "red")) +
+        theme(legend.position="top")
 
     graph02 <- ggplot(data, aes(x=nbLinkOneLink)) +
         geom_histogram(colour = "black",fill = c("black"), bins=bins) +
@@ -34,5 +41,13 @@ plot.subnetwork <- function(x, bins=30, ...) {
         scale_color_manual(name = "", values = c(observed = "red")) +
         theme(legend.position="bottom")
 
-    grid.arrange(graph01, graph02, graph03, nrow=3)
+    g_legend<-function(a.gplot){
+        tmp <- ggplot_gtable(ggplot_build(a.gplot))
+        leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+        legend <- tmp$grobs[[leg]]
+        return(legend)}
+
+    legend <- g_legend(graphTEMP)
+
+    grid.arrange(graph01, graph02, graph03, legend, nrow=4, heights=c(4, 4, 4, 1))
 }

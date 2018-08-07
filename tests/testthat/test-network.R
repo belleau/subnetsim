afb68b87bea1cbd1a9b7b5391e9676445dfd0861
Astrid Deschenes <adeschen@hotmail.com>
2018-08-06 21:36:27 -0400

    Adding unit tests

--- tests/testthat/test-network.R
+++ tests/testthat/test-network.R
@@ -1,17 +1,28 @@
-### Unit tests for subnetsimInternalMethods.R functions
+### Unit tests for network.R functions
 
-# library(subnetsim)
-#
-# load(system.file("extdata", "demo_netAll.RData",
-#                  package = "subnetsim"))
-#
-#
-# ### Tests network() results
-#
-# context("network() results")
-#
-#
-# test_that("network() must return error when fileName problem") {
-#     netFileName <- "bla.sif"
-#     error_message <- paste0("Problem with network filename ", netFileName)
-# }
+library(subnetsim)
+
+networkFile <- system.file("extdata", "demo_network.sif",  package="subnetsim")
+
+### Tests network() results
+
+
+context("network() results")
+
+
+test_that("network() must return error when fileName is not supported", {
+     fileType <- "tif"
+     error_message <- paste0("File type ", fileType, " is not supported")
+
+     expect_error(network(netFileName = "test.tif", fileType = fileType,
+                          subNetFileName=NULL), error_message)
+})
+
+test_that("network() returns predicted results ", {
+    netTest <- network(netFileName = networkFile, fileType = "sif",
+                       subNetFileName=NULL)
+
+    expect_null(netTest$nodesSubNet)
+    expect_equal(length(netTest$nodesAll), 313)
+    expect_true(is.hash(netTest$netAll))
+})
